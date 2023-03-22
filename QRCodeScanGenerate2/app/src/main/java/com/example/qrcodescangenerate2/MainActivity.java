cedbd42d80ccb9244c925e03312468c4572e395b
nasaro112702 <62674627+nasaro112702@users.noreply.github.com>
2023-03-23 06:42:43 +0800

    .

--- QRCodeScanGenerate2/app/src/main/java/com/example/qrcodescangenerate2/MainActivity.java
+++ QRCodeScanGenerate2/app/src/main/java/com/example/qrcodescangenerate2/MainActivity.java
@@ -1,14 +1,83 @@
 package com.example.qrcodescangenerate2;
 
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.DialogInterface;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.EditText;
+import android.widget.ImageView;
+
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.MultiFormatWriter;
+import com.google.zxing.WriterException;
+import com.google.zxing.common.BitMatrix;
+import com.journeyapps.barcodescanner.BarcodeEncoder;
+import com.journeyapps.barcodescanner.ScanContract;
+import com.journeyapps.barcodescanner.ScanOptions;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText text;
+    ImageView result;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        text = findViewById(R.id.et_text);
+        result = findViewById(R.id.img_result);
     }
+
+    public void generateCode(View view){
+        String input = text.getText().toString();
+
+        MultiFormatWriter writer = new MultiFormatWriter();
+
+        try {
+            BitMatrix matrix = writer.encode(input, BarcodeFormat.QR_CODE, 350, 350);
+
+            BarcodeEncoder encoder = new BarcodeEncoder();
+
+            Bitmap bitmap = encoder.createBitmap(matrix);
+
+            result.setImageBitmap(bitmap);
+
+            InputMethodManager manager = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+
+            manager.hideSoftInputFromWindow(text.getApplicationWindowToken(), 0);
+
+        } catch (WriterException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void scanCode(View view){
+        ScanOptions options = new ScanOptions();
+        options.setOrientationLocked(true);
+        options.setCaptureActivity(CaptureAct.class);
+        options.setBeepEnabled(true);
+        options.setPrompt("Place the camera to the code");
+        barlauncher.launch(options);
+    }
+
+    ActivityResultLauncher<ScanOptions> barlauncher = registerForActivityResult(new ScanContract(), result -> {
+        if(result.getContents() != null){
+            AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+            builder.setTitle("Result");
+            builder.setMessage(result.getContents());
+            builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    dialog.dismiss();
+                }
+            }).show();
+        }
+    });
 }
