f91ce7d59b8d632667ad7b8370d6bfe91fa800b3
nasaro112702 <62674627+nasaro112702@users.noreply.github.com>
2023-04-10 14:58:28 +0800

    Merge remote-tracking branch 'origin/main'

--- AttendanceQRCode/app/src/main/java/com/example/attendanceqrcode/HomeActivity.java
+++ AttendanceQRCode/app/src/main/java/com/example/attendanceqrcode/HomeActivity.java
@@ -1,14 +1,42 @@
 package com.example.attendanceqrcode;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class HomeActivity extends AppCompatActivity {
+
+    BottomNavigationView bottomNavigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        bottomNavigationView = findViewById(R.id.bottomNavigationView);
+
+        getSupportFragmentManager().beginTransaction().replace(R.id.frameLayout, new HomeFragment()).commit();
+
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch(item.getTitle().toString()){
+                    case "Home":
+                        getSupportFragmentManager().beginTransaction().replace(R.id.frameLayout, new HomeFragment()).commit();
+                        break;
+                    case "Attendance Checker":
+                        getSupportFragmentManager().beginTransaction().replace(R.id.frameLayout, new ScanFragment()).commit();
+                        break;
+                    case "Students":
+                        getSupportFragmentManager().beginTransaction().replace(R.id.frameLayout, new StudentsFragment()).commit();
+                        break;
+                }
+                return true;
+            }
+        });
     }
 }
