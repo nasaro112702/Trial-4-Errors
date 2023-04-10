d43a35730e8233ff737125d211a962ee2d9fb489
nasaro112702 <62674627+nasaro112702@users.noreply.github.com>
2023-04-10 14:08:59 +0800

    .

---
+++ AttendanceQRCode/app/src/main/java/com/example/attendanceqrcode/DatabaseHelper.java
@@ -0,0 +1,43 @@
+package com.example.attendanceqrcode;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import androidx.annotation.Nullable;
+
+public class DatabaseHelper extends SQLiteOpenHelper {
+
+    SQLiteDatabase db = getWritableDatabase();
+    public DatabaseHelper(@Nullable Context context) {
+        super(context, "attendance", null, 1);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL("create table users (id int primary key autoincrement, firstname text, lastname text, username text, password text)");
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("drop table if exists users");
+    }
+
+    void addUser(String firstname, String lastname, String username, String password){
+        ContentValues cv = new ContentValues();
+        cv.put("firstname", firstname);
+        cv.put("lastname", lastname);
+        cv.put("username", username);
+        cv.put("password", password);
+
+        db.insert("users", null, cv);
+    }
+
+    Cursor getValue(String query){
+        return db.rawQuery(query, null);
+    }
+
+
+}
